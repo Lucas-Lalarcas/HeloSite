c2f3941c363ef405d0847989c7cfc5f73a1e9cdb
Lucas-Lalarcas <lucasbarbosa0217@gmail.com>
2022-01-26 16:51:25 -0300

    versão final

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 function atualizaTela(num){
     if(canwe){
-    tela.innerHTML = tela.innerHTML + (num);}
+        if(tela.innerHTML.length < 10){
+    tela.innerHTML = tela.innerHTML + (num);}}
   }
 
-
+  
 var btn1 = document.getElementById('1'); 
 var btn2 = document.getElementById('2'); 
 var btn3 = document.getElementById('3'); 
@@ -14,44 +15,75 @@ var btn7 = document.getElementById('7');
 var btn8 = document.getElementById('8'); 
 var btn9 = document.getElementById('9'); 
 var btn0 = document.getElementById('0'); 
+var btnponto = document.getElementById('ponto'); 
 var btnmais = document.getElementById('+');
+var btnmenos = document.getElementById('-');
+var btnvezes = document.getElementById('x');
+var btndivide = document.getElementById('/');
 var btnreset = document.getElementById('reset');
 var btnigual = document.getElementById('=');
 var tela = document.getElementById('screen');
-var resultado = 0;
-var type;
 var canwe = true;
-var base = [];
+var pode = true;
+var valor1 = 0;
+var valor2 = 0;
+var tipo1 = "";
 
-btnmais.addEventListener("click", function(){
-    if(canwe){{if(tela.innerHTML == ""){window.alert("digite antes de somar!")}else{
-    base.push(parseInt(tela.innerHTML) );
-    tela.innerHTML = "";}
-}}});
+btnmais.addEventListener("click" , function(){
+    opera("+");
+})
 
-btnreset.addEventListener("click", function(){
-    if(tela.innerHTML == ""){window.alert("digite antes de resetar")}else{
-    base = [];
-    resultado = 0;
-    tela.innerHTML = "";
-    canwe = true;
+btnmenos.addEventListener("click" , function(){
+    opera("-");
+})
+
+btnvezes.addEventListener("click" , function(){
+    opera("x");
+})
+
+btndivide.addEventListener("click" , function(){
+    opera("/");
+})
+
+btnigual.addEventListener("click" , function(){
+    iguala();
+})
+
+
+function opera(tipo){
+    if(pode && canwe){
+        valor1 = parseFloat(tela.textContent.replace(",", "."));
+        tipo1 = tipo;
+        pode = false;
+        tela.innerHTML = "";
+        iguale = true;
+    }
 }
-});
 
+function iguala(){
+    if(canwe && iguale){
+        valor2 = parseFloat(tela.textContent.replace(",", "."));
+        if(tipo1 == "+") {
+        tela.innerHTML =(valor1+valor2);
+    }
+    else if(tipo1 == "-") {
+        tela.innerHTML =  (valor1-valor2);
+    }
+    else if(tipo1 == "x") {
+        tela.innerHTML =  (valor1*valor2);
+    }
+    else if(tipo1 == "/") {
+        tela.innerHTML = (valor1/valor2);
+    }
+    else {tela.innerHTML = "ERRO"}
+    canwe = true;
+    pode = true;
+    iguale = false;
+}
+}
 
+iguale = false;
 
-
-btnigual.addEventListener("click", function(){
-    if(canwe){
-    if(tela.innerHTML == ""){window.alert("digite antes de saber o resultado!")}else{
-        base.push(parseInt(tela.innerHTML) *1);
-        tela.innerHTML = "";}
-        for(var i = 0; i < base.length; i++){
-            resultado = resultado + base[i];
-        }
-        tela.innerHTML = "Resultado = "+resultado
-        canwe = false;
-}});
 
 
 btn1.addEventListener("click", function(){
@@ -95,12 +127,28 @@ btn9.addEventListener("click", function(){
 });
 
 btn0.addEventListener("click", function(){
-    if(tela.innerHTML == ""){console.log("Para de colocar 0 na esquerda seu idiota")} else{
+    if(tela.innerHTML == ""){console.log("Para de colocar 0 na esquerda")} else{
     atualizaTela(0)}
+});
+
+
+btnponto.addEventListener("click", function(){
+    if(tela.innerHTML == ""){console.log("Para de colocar . na esquerda")} else{
+    atualizaTela('.')}
 });
 
 
 
 
+btnreset.addEventListener("click", function(){
+    valor1 = 0;
+    tipo1 = "";
+    valor2 = 0;
+    canwe = true;
+    pode = true;
+    tela.innerHTML = "";
+    iguale = false;
+
+})
 
 
